7f97d47240478e2b848833e9cfdcd892d94b293c
Hemanvitha_Karnatapu <34389813+Hemanvitha@users.noreply.github.com>
2021-08-08 14:24:08 -0700

    Add files via upload

---
+++ web/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.SimilarityDB
+users = db['users']
+
+class Register(Resource):
+
